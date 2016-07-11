a5c2270f438d1d952237fc6c6b49b6cfb18af465
Yeiber Cano <yeibercano@gmail.com>
2016-07-10 23:19:17 -0700

    install and import axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
 import ApiKey from '../secrets';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ApiKey.API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
