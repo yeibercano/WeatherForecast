e653a02408d970baa8614af47d7c783f4bcf2776
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 02:38:40 -0700

    add avg temp line and units to sections

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+function average(data) {
+	return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
 	return (
 		<div>
 		  <Sparklines height={120} width={180} data={props.data}>
 			  <SparklinesLine color={props.color} />
-				</Sparklines>
+			  <SparklinesReferenceLine type='avg' />
+			</Sparklines>
+			<div>{average(props.data)} {props.units}</div>
 		</div>
 	)
 }
