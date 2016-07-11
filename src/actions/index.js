28b502c8f0e3c0ba3b6cb8d746adddefb9dd9ddb
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 01:44:28 -0700

    create a city section

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
-
+	
 	return {
 		type: FETCH_WEATHER,
 		payload: request
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
  class WeatherList extends Component {
+ 	renderWeather(cityData) {
+ 		const name = cityData.city.name;
+ 		return (
+ 			<tr key={name}>
+ 				<td>{name}</td>
+ 			</tr>
+ 		)
+ 	}
 	render() {
 		return (
 			<table className='table table-hover'>
@@ -14,6 +22,7 @@ import { connect } from 'react-redux';
 			  	</tr>
 			  </thead>
 			  <tbody>
+			    {this.props.weather.map(this.renderWeather)}
 			  </tbody>
 			</table>
 
