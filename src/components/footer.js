6fd8caa6852bcb96768badc08d4d2c1aaee7b7ac
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 21:10:54 -0700

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+ 
+	<footer className='footer'>
+	  <div className='container'>
+		  <p>Footer 2016</p>
+		</div>
+	</footer>
+)
+
+export default Footer
