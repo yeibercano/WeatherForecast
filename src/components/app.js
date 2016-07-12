872de99a16c5044147db91a2938c74c02383f593
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 20:11:04 -0700

    add title

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <div> <h1 className='title'>Five-day Weather Forecast </h1></div>
         <SearchBar />
         <WeatherList />
       </div>
