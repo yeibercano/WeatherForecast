0af7f5502a3717ffeb2298535b8f84408a2157f9
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 21:11:34 -0700

    import footer and header

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
+import Header from './header';
+import Footer from './footer';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <div> <h1 className='title'>Five-day Weather Forecast </h1></div>
         <SearchBar />
         <WeatherList />
+        <Footer />
       </div>
     );
   }
