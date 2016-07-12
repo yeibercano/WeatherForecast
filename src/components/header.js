3c1fc52c7765d33c269b3aea241fb2770a09f990
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 21:11:12 -0700

    add header

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+	<header>
+	 <div className='header-wrapper container'>
+	   <div className="brand">FiveDay</div>
+	   <nav >
+	    <ul>
+	      <li><a href="#" title="home">home</a></li>
+	    </ul>
+	   </nav>
+	 </div>
+	</header>
+)
+
+export default Header
