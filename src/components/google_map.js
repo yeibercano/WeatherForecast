7bf4f1a9ca0c2a528b96e9b654221ab07b3d6a96
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 20:11:31 -0700

    fix typo in default

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -7,7 +7,7 @@ export default (props) => {
 		<GoogleMapLoader
 		  containerElement={ <div style={{height: '100%'}} /> }
 		  googleMapElement={
-		  	<GoogleMap feaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}}
+		  	<GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon }} />
 		  }
 		/>
 	)
